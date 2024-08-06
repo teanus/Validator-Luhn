c663053f2041e2cc908fb943e96a8e20e8f1f99a
teanus <Gamer.57dima@gmail.com>
2024-08-06 22:41:50 +0500

    delete

--- example.py
+++
@@ -1,5 +0,0 @@
-from validatorLuhn import validate
-
-print(validate("2200 8373 8276 4455"))  # Указан неверный номер карты, вернет False
-print(validate("4276 8070 1492 79 48"))  # Указан верный номер карты, вернет True
-
